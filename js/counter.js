ff29a095e21d85ccc5ca4f400858d9b7cc76ca80
Michael Töpfer <micha.toepfer@googlemail.com>
2024-04-15 15:13:21 +0200

    creat counter js

---
+++ js/counter.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const textareas = document.querySelectorAll("textarea");
+  const charCounts = document.querySelectorAll("[data-js^='charCount']");
+
+  textareas.forEach(function (textarea, index) {
+    const charCount = charCounts[index];
+
+    // Funktion zum Zählen der verbleibenden Zeichen
+    function countCharacters() {
+      const remainingChars = 150 - textarea.value.length;
+      charCount.textContent = +remainingChars;
+    }
+
+    // Eventlistener für Tastatureingabe und Einfügen von Text
+    textarea.addEventListener("input", countCharacters);
+
+    // Initialen Aufruf, um den Zähler zu aktualisieren
+    countCharacters();
+  });
+});
